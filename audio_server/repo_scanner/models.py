017b3a2cbf6bc01b49a355f0714ec07ac38afe18
Dan Harty <dan.john.harty@gmail.com>
2019-02-03 21:43:53 +0100

    Config - AudioAsset update
    Include addition AudioAsset metadata

--- audio_server/repo_scanner/models.py
+++ audio_server/repo_scanner/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class AudioAsset(models.Model):
     filename = models.CharField(max_length=100)
     filepath = models.CharField(max_length=300)
+    scandate = models.DateTimeField(auto_now=True)
+    bitrate = models.IntegerField(null=True)
+    length = models.FloatField(null=True)
+    samplerate = models.IntegerField(null=True)
+    lufs = models.FloatField(null=True)
+    channels = models.IntegerField(null=True)
+    crc = models.CharField(null=True, max_length=100)
